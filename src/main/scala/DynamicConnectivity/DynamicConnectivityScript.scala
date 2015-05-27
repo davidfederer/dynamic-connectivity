6f1f184ad7b4a1d112109b0a121e2e4424689c76
David Federer <David.Federer@colesfinancialservices.com.au>
2015-05-27 15:41:15 +1000

    ecid hashmap added

--- src/main/scala/DynamicConnectivity/DynamicConnectivityScript.scala
+++ src/main/scala/DynamicConnectivity/DynamicConnectivityScript.scala
@@ -6,6 +6,8 @@
 import java.util.UUID
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.sql.types.{StructType, StructField, StringType};
 
 
 private val parent = mutable.HashMap[String, String]()
@@ -13,8 +15,9 @@ private val parent = mutable.HashMap[String, String]()
 private val size = mutable.HashMap[String, Int]()
   //number of objects in subtree rooted at i
 private val cluster = mutable.HashMap[String, String]()
+//private val component = mutable.HashMap[String, String]()
 private var count = 0 // number of components
-
+private val ecid_map = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(map(x => (x._1, x._2)))
 //Returns the number of components
 def countComponent(): Int = {
   return count
@@ -68,30 +71,40 @@ where each integer represents some object
 if the objects are in different components merge the two components*/
 
 
-case class prs(id0: String, id1: String)
 
 //val pairs = List(List("1", "2"), List("3", "1"), List("3", "4"), List("5", "6"), List("7", "8"))
-val pairs = sqlContext.sql("""SELECT id0, id1 FROM lighthouse_sandpit.customermatching_pow1_orc""").map(
-  col => prs(col(0).asInstanceOf[String], col(1).asInstanceOf[String])).map(x => {Array(x.id0, x.id1).mkString(",")}).toList
+val pairs = sqlContext.sql("SELECT id0, id1 FROM lighthouse_sandpit.customermatching_pow1_orc WHERE match_score >= 0.8").map(
+  row => (row(0).asInstanceOf[String], row(1).asInstanceOf[String])
+)
 
-for (i <- 0 until pairs.count) {
-  val p = pairs(i)(0)
-  val q = pairs(i)(1)
+for (i <- pairs.toArray) {
+  val p = i._1
+  val q = i._2
 
   checkIn(p)
   checkIn(q)
 
   if (!connected(p, q)) {
     union(p, q)
-    println(p, q)
+    //println(p, q)
   }
 }
 
+
+val p = ArrayBuffer[(String, String, String)]()
+
 for (i <- parent.keys) {
-  println(i + ", " + cluster(find(i)))
+  val t = (cluster(find(i)), ecid_map(i), i)
+  p += t
 }
 
-println("number of clusters: " + count)
+
+
+case class cmp(uuid: String, ecid: String, id: String)
+sc.parallellize(p).map(x => {cmp(x._1, x._2, x._3)}).toDF.saveAsParquet("/data/cfs/dev01/work/custm/components")
+
+
+//println("number of clusters: " + count)
 
 
 def checkIn(site: String): Unit = {
@@ -105,4 +118,3 @@ def checkIn(site: String): Unit = {
 
 
 
-//.coalesce(1, true).saveAsTextFile("<path>")
--- src/main/scala/Party/PartyScript.scala
+++ src/main/scala/Party/PartyScript.scala
@@ -1,5 +1,5 @@
 import scala.collection.mutable.ArrayBuffer
-import scala.collection.parallel.mutable
+
 
 var set_new_ecid = ArrayBuffer[List[(String, String)]]()
 
@@ -7,7 +7,7 @@ def compare(cOld: List[(String, String)], cNew: List[(String, String)]): Unit = 
 
   val cNew_filtered = cNew.filter(x => x._1 == "") // identify pairs with no previous ECID
 
-  if (cNew.intersect(cOld).isEmpty && cNew_filtered.size != cNew.size) {return} // do not compare clusters that have not set relations
+  if (cNew.intersect(cOld).isEmpty && cNew_filtered.size != cNew.size) {return} // do not compare clusters that have no set relations
   else {
 
     if (cNew.diff(cNew_filtered).size == cOld.size && cNew.diff(cNew_filtered).diff(cOld).isEmpty) { //if two clusters completely overlap or pairs with no previous ECID were added to the historic cluster...
@@ -21,56 +21,38 @@ def compare(cOld: List[(String, String)], cNew: List[(String, String)]): Unit = 
   }
 }
 
-    //historic set of clusters
-    val cluster_old = List(("d2d9d98b-1730-4460-9fbd-94c4d485bf12", "94c4d485bf12", "cp678"),
-      ("d2d9d98b-1730-4460-9fbd-94c4d485bf12", "94c4d485bf12", "cp856"),
-      ("85d2700f-a6c1-4fed-9b13-bca8955fcfcc", "bca8955fcfcc", "ct789"),
-      ("e870d741-b70e-4001-98e6-f24862ba60f1", "f24862ba60f1", "ct567"),
-      ("e870d741-b70e-4001-98e6-f24862ba60f1", "f24862ba60f1", "ct478"),
-      ("e870d741-b70e-4001-98e6-f24862ba60f1", "f24862ba60f1", "ct456"),
-      ("e870d741-b70e-4001-98e6-f24862ba60f1", "f24862ba60f1", "ct123"))
 
-    //new set of clusters
-    val cluster_new = List(("d2d9d98b-1730-4460-9fbd-94c4d485bf12", "94c4d485bf12", "cp678"), //SCENARIO 1 (m/b rules 1, 8)
-      ("d2d9d98b-1730-4460-9fbd-94c4d485bf12", "94c4d485bf12", "cp856"),
-      ("d2d9d98b-1730-4460-9fbd-94c4d485bf11", "", "cp350"), // SCENARIO 2 (m/b rules 2, 5)
-      ("85d2700f-a6c1-4fed-9b13-bca8955fcfcc", "", "cp345"), // SCENARIO 3 (m/b rule 3)
-      ("85d2700f-a6c1-4fed-9b13-bca8955fcfcc", "bca8955fcfcc", "ct789"),
-      ("e870d741-b70e-4001-98e6-f24862ba60f2", "f24862ba60f1", "ct567"), //SCENARIO 4 (m/b rules 4, 6, 7, 10)
-      ("e870d741-b70e-4001-98e6-f24862ba60f2", "f24862ba60f1", "ct478"),
-      ("e870d741-b70e-4001-98e6-f24862ba60f1", "f24862ba60f1", "ct456"),
-      ("e870d741-b70e-4001-98e6-f24862ba60f1", "f24862ba60f1", "ct124"))
+val set_old = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(x => (x._1, x._2, x._3)).groupBy(x => x._1).map(x => {
+  //group by cluster ID and remove UUID
+  x._2.map(x => (x._2, x._3))
+}).toList
 
-    val set_old = cluster_old.groupBy(x => x._1).map(x => { //group by cluster ID and remove UUID
-      x._2.map(x => (x._2, x._3))
-    }).toList
-
-    val set_new = cluster_new.groupBy(x => x._1).map(x => { //group by cluster ID and remove UUID
-      x._2.map(x => (x._2, x._3))
-    }).toList
+val set_new = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(x => (x._1, x._2, x._3)).groupBy(x => x._1).map(x => {
+  //group by cluster ID and remove UUID
+  x._2.map(x => (x._2, x._3))
+}).toList
 
 
-    var outerloop: Int = 1
-    var innerloop: Int = 1
+var outerloop: Int = 1
+var innerloop: Int = 1
 
-    if(set_new.size > set_old.size){
-      outerloop = set_old.size
-      innerloop = set_new.size
-    }
-
-    else {
-      outerloop = set_new.size
-      innerloop = set_old.size
-    }
-
-    for (i <- 0 until outerloop) {
-      for (j <- 0 until innerloop) {
-        //println(set_old(i), set_new(j), "(" + i, j + ")")
-        compare(set_old(i), set_new(j))
-      }
-    }
-    println(set_new_ecid.distinct)
-  }
+if(set_new.size > set_old.size){
+  outerloop = set_old.size
+  innerloop = set_new.size
 }
+else {
+  outerloop = set_new.size
+  innerloop = set_old.size
+}
+
+for (i <- 0 until outerloop) {
+  for (j <- 0 until innerloop) {
+   compare(set_old(i), set_new(j))
+   }
+}
+
+case class cmp(ecid: String, id: String)
+sc.parallellize(set_new_ecid.distinct).map(x => {cmp(x._1, x._2}).toDF.saveAsParquet("/data/cfs/dev01/work/custm/components")
+
 
 
