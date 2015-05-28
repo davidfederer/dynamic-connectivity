0882b07905798abbd7b9efb2f6e86e86029df3c2
David Federer <David.Federer@colesfinancialservices.com.au>
2015-05-28 15:42:39 +1000

    to be tested

--- src/main/scala/DynamicConnectivity/DynamicConnectivityScript.scala
+++ src/main/scala/DynamicConnectivity/DynamicConnectivityScript.scala
@@ -9,15 +9,13 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.sql.types.{StructType, StructField, StringType};
 
+val parent = mutable.HashMap[String, String]()  //parent if i
+val size = mutable.HashMap[String, Int]()  //number of objects in subtree rooted at i
+val cluster = mutable.HashMap[String, String]() //private val component = mutable.HashMap[String, String]()
+var count = 0 // number of components
+val ecid_map = mutable.HashMap[String, String]() //creates key value pairs for client ids and ecids
 
-private val parent = mutable.HashMap[String, String]()
-  //parent if i
-private val size = mutable.HashMap[String, Int]()
-  //number of objects in subtree rooted at i
-private val cluster = mutable.HashMap[String, String]()
-//private val component = mutable.HashMap[String, String]()
-private var count = 0 // number of components
-private val ecid_map = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(map(x => (x._1, x._2)))
+
 //Returns the number of components
 def countComponent(): Int = {
   return count
@@ -66,38 +64,35 @@ def union(p: String, q: String): Unit = {
 }
 
 
-/*Reads in a sequence of pairs of integers (between 0 and N-1) from standard input
-where each integer represents some object
-if the objects are in different components merge the two components*/
-
-
-
-//val pairs = List(List("1", "2"), List("3", "1"), List("3", "4"), List("5", "6"), List("7", "8"))
-val pairs = sqlContext.sql("SELECT id0, id1 FROM lighthouse_sandpit.customermatching_pow1_orc WHERE match_score >= 0.8").map(
-  row => (row(0).asInstanceOf[String], row(1).asInstanceOf[String])
+val new_pairs = sqlContext.sql("SELECT id0, id1, ecid FROM lighthouse_sandpit.customermatching_pow1_orc WHERE match_score >= 0.8").map(
+  row => (row(0).asInstanceOf[String], row(1).asInstanceOf[String], row(2).asInstanceOf[String])
 )
 
-for (i <- pairs.toArray) {
+for (i <- new_pairs.toArray) {
   val p = i._1
   val q = i._2
+  val r = i._3
 
-  checkIn(p)
-  checkIn(q)
+  checkIn(p, r)
+  checkIn(q, r)
 
   if (!connected(p, q)) {
     union(p, q)
-    //println(p, q)
   }
 }
 
 
-val p = ArrayBuffer[(String, String, String)]()
+//val p = ArrayBuffer[(String, String, String)]()
+//
+//for (i <- parent.keys) {
+//  val t = (cluster(find(i)), cid_map(i), i)
+//  p += t
+//}
 
-for (i <- parent.keys) {
-  val t = (cluster(find(i)), ecid_map(i), i)
-  p += t
-}
 
+val p = parent.keys.map(
+  x => {(cluster(find(x)), {if (!ecid_map(x).isEmpty) ecid_map(x) else ""}, x)}
+)
 
 
 case class cmp(uuid: String, ecid: String, id: String)
@@ -107,12 +102,13 @@ sc.parallellize(p).map(x => {cmp(x._1, x._2, x._3)}).toDF.saveAsParquet("/data/c
 //println("number of clusters: " + count)
 
 
-def checkIn(site: String): Unit = {
+def checkIn(site: String, ecid: String): Unit = {
   if(!parent.contains(site)) {
     parent(site) = site
     size(site) = 1
     cluster(site) = UUID.randomUUID().toString
     count = count + 1
+    ecid_map(site) = ecid
   }
 }
 
--- src/main/scala/Party/PartyScript.scala
+++ src/main/scala/Party/PartyScript.scala
@@ -1,36 +1,38 @@
 import scala.collection.mutable.ArrayBuffer
 
 
-var set_new_ecid = ArrayBuffer[List[(String, String)]]()
+var set_new_ecid = ArrayBuffer[List[(String, String, String)]]()
 
-def compare(cOld: List[(String, String)], cNew: List[(String, String)]): Unit = {
+def compare(cOld: List[(String, String, String)], cNew: List[(String, String, String)]): Unit = {
 
-  val cNew_filtered = cNew.filter(x => x._1 == "") // identify pairs with no previous ECID
+  val cNew_filtered = cNew.filter(x => x._2 == "") // identify pairs with no previous ECID
 
   if (cNew.intersect(cOld).isEmpty && cNew_filtered.size != cNew.size) {return} // do not compare clusters that have no set relations
   else {
 
     if (cNew.diff(cNew_filtered).size == cOld.size && cNew.diff(cNew_filtered).diff(cOld).isEmpty) { //if two clusters completely overlap or pairs with no previous ECID were added to the historic cluster...
-      val ecId = cOld(0)._1
-        set_new_ecid += cNew.map(x => (ecId, x._2)) //..assign the historic ECID ...
+      val ecId = cOld(0)._2
+        set_new_ecid += cNew.map(x => (x._1, ecId, x._3)) //..assign the historic ECID ...
     }
 
     else {
-      set_new_ecid += cNew.map(x => ("new ECID", x._2)) //... else generate a new ECID
+      set_new_ecid += cNew.map(x => (x._1, "Assign New ECID", x._3)) //... else generate a new ECID
     }
   }
 }
 
+//val set_old = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(x => (x._1, x._2, x._3)).groupBy(x => x._1).map(x => {
+//  //group by cluster ID and remove UUID
+//  x._2.map(x => (x._2, x._3))
+//}).toList
 
-val set_old = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(x => (x._1, x._2, x._3)).groupBy(x => x._1).map(x => {
-  //group by cluster ID and remove UUID
-  x._2.map(x => (x._2, x._3))
-}).toList
+val set_old = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(x => (x._1, x._2, x._3)).groupBy(x => x._1).map(
+  x => x._2
+).toList //group by cluster ID and remove UUID
 
-val set_new = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(x => (x._1, x._2, x._3)).groupBy(x => x._1).map(x => {
-  //group by cluster ID and remove UUID
-  x._2.map(x => (x._2, x._3))
-}).toList
+val set_new = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components").map(x => (x._1, x._2, x._3)).groupBy(x => x._1).map(
+  x => x._2
+).toList //group by cluster ID and remove UUID
 
 
 var outerloop: Int = 1
@@ -51,8 +53,8 @@ for (i <- 0 until outerloop) {
    }
 }
 
-case class cmp(ecid: String, id: String)
-sc.parallellize(set_new_ecid.distinct).map(x => {cmp(x._1, x._2}).toDF.saveAsParquet("/data/cfs/dev01/work/custm/components")
+case class cmp(uuid: String, ecid: String, id: String)
+sc.parallellize(set_new_ecid.distinct).map(x => {cmp(x._1, x._2, x._3}).toDF.saveAsParquet("/data/cfs/dev01/work/custm/components")
 
 
 
