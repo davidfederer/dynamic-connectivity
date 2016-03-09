00d6c68acf604c9927b254dc2aa545ccc6f850fe
David Federer <David.Federer@colesfinancialservices.com.au>
2016-03-09 15:13:00 +1100

    0.9 version

--- build.sbt
+++ build.sbt
@@ -7,6 +7,19 @@ scalaVersion := "2.11.5"
 // Change this to another test framework if you prefer
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
+
+val droolsVersion = "6.0.1.Final"
+
+resolvers += "JBoss public" at "http://repository.jboss.org/nexus/content/groups/public/"
+
+libraryDependencies ++= {
+  "org.kie" % "kie-api" % droolsVersion ::
+    List("drools-compiler", "drools-core", "drools-jsr94", "drools-decisiontables", "knowledge-api")
+      .map("org.drools" % _ % droolsVersion)
+}
+
+
+
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.3.9"
 
--- src/main/scala/Party/PartyScript.scala
+++ src/main/scala/Party/PartyScript.scala
@@ -11,21 +11,21 @@ def compare(o: Iterable[cmp], n: Iterable[cmp]): Array[(String, Long, String)] =
   }
   else {
     //        set_new_ecid += cNew.map(x => (x._1, -1L, x._3)) //... else generate a new ECID
-    return cNew.map(x => (x._1,scala.math.abs(new scala.collection.immutable.StringOps(x._1).hashCode()).toLong, x._3)) //... else generate a new ECID
+    return cNew.map(x => (x._1,{
+      val i = scala.math.abs(new scala.collection.immutable.StringOps(x._1).hashCode()).toLong
+      if (i % 3 == 0) {
+        -1
+      } else { i }
+    }
+      , x._3)) //... else generate a new ECID
   }
 }
-//{
-//  val i = scala.math.abs(new scala.collection.immutable.StringOps(x._1).hashCode()).toLong
-//  if (i % 3 == 0) {
-//    -1
-//  } else { i }
-//}
-//
+
 def hasNoSetRelation(o: Iterable[cmp], n: Iterable[cmp]): Boolean = { // do not compare clusters that have no set relations
 
   val cOld = o.map(x => (x.ecid, x.id)).toArray
   val cNew = n.map(x => (x.ecid, x.id)).toArray
-  val cNew_filtered = cNew.filter(x => x._1 == -1) // identify pairs with no previous ECID
+  val cNew_filtered = cNew.filter(x => x._1 == -1)
   return (cNew.intersect(cOld).isEmpty /* && !cNew.diff(cNew_filtered).isEmpty*/)
 }
 
@@ -42,6 +42,3 @@ val set_new = sqlContext.parquetFile("/data/cfs/dev01/work/custm/components-1").
 val res = set_old.cartesian(set_new).filter(x => {hasNoSetRelation(x._1, x._2) == false}).distinct.map(x => compare(x._1, x._2)).map(x => {cmp(x(0).asInstanceOf[String], x(1).asInstanceOf[Long], x(2).asInstanceOf[String])}).toDF()
 
 res.saveAsParquetFile("/data/cfs/dev01/work/custm/components")
-
-
-
